a00f8000ad0334b7700919c5eceab7afc553933d
yueagar <kelvin20040807@gmail.com>
2024-10-05 22:38:40 +0800

    Phonebook fullstack

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const middleware = morgan(":method :url :status :res[content-length] - :response-time ms :body");
@@ -42,6 +43,7 @@ const generateId = () => {
 
 app.use(express.json());
 app.use(middleware);
+app.use(cors());
 
 app.get("/info", (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
@@ -91,6 +93,6 @@ app.post("/api/persons", (request, response) => {
     response.json(person);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
